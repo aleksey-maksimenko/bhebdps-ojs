f80a3744b88ac944c7f9ccd369d518cf3153c3ad
aleksey-maksimenko <62542456+aleksey-maksimenko@users.noreply.github.com>
2024-11-13 15:22:27 +0300

    Выполнено 3.2 Навигац. меню (+1 доп)

--- Домашние задания/3.2 Навигационное меню/task.js
+++ Домашние задания/3.2 Навигационное меню/task.js
@@ -1,15 +1,25 @@
 const linksFromMenu = Array.from(document.querySelectorAll('.menu__link'));
 
-linksFromMeny.forEach(link => {
-    link.addEventListener('click', function(event) {
-        const item = this.closest('.menu__item');
-        const submenu = item.querySelector('.menu_sub');
+linksFromMenu.forEach(link => {
+  link.addEventListener('click', function(event) {
+    const item = this.closest('.menu__item');
+    const submenu = item.querySelector('.menu_sub');
 
-        if (submenu) {
-            submenu.classList.toggle('menu_active');             
-            // предотвращения перехода
-            //return false;
-			event.preventDefault(); 			
+    if (submenu) {
+      event.preventDefault();
+
+      const otherSubs = document.querySelectorAll('.menu_sub.menu_active');
+      otherSubs.forEach(otherSub => {
+        if (otherSub !== submenu) {
+          otherSub.classList.remove('menu_active'); 
         }
-    });
+      });
+
+      if (submenu.classList.contains('menu_active')) {
+        submenu.classList.remove('menu_active'); 
+      } else {
+        submenu.classList.add('menu_active'); 
+      } 
+    }
+  });
 });
