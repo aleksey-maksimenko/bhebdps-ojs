c39bf708ab81ca4836c1410e82383d14fdd14138
aleksey-maksimenko <62542456+aleksey-maksimenko@users.noreply.github.com>
2024-12-06 09:42:01 +0300

    3.4 Анимация загрузки - Реализация кэширования
    Реализация кэширования (доп. задание) в ДЗ с Анимацией загрузки
    При обновлении страницы будут показываться ранее загруженные данные из LocalStorage

--- Домашние задания/3.4 Анимация загрузки данных/task.js
+++ Домашние задания/3.4 Анимация загрузки данных/task.js
@@ -5,33 +5,44 @@ async function loadCurrencyRates() {
   try {
     loader.classList.add('loader_active');
 
+    const prevCurrencies = localStorage.getItem('currencyRates');
+    
+    if (prevCurrencies) { // отображаем кэшированные данные
+      printCurrencies(JSON.parse(prevCurrencies).response.Valute);
+    }
+
     const response = await fetch('https://students.netoservices.ru/nestjs-backend/slow-get-courses');
     if (!response.ok) { // если не получили нужный ответ сервера
       throw new Error('Неудалось загрузить курсы валют');
     }
 
-    // разбор json-ответа
     const data = await response.json();
-    const currencies = data.response.Valute;
-    for (const key in currencies) {
-      if (currencies.hasOwnProperty(key)) {
-        const currency = currencies[key];
-
-        const divItem = document.createElement('div');
-        divItem.className = 'item';
-
-        divItem.innerHTML = `
-          <div class="item__code">${currency.CharCode}</div>
-          <div class="item__value">${currency.Value.toFixed(2)}</div>
-          <div class="item__currency">руб.</div>
-        `;
-        itemsTable.appendChild(divItem);
-      }
-    }
+    localStorage.setItem('currencyRates', JSON.stringify(data)); // заносим в localStorage
+    printCurrencies(data.response.Valute);
   } catch (error) {
     console.error('Ошибка при загрузке данных:', error);
   } finally { // скрытие загрузки
     loader.classList.remove('loader_active');
+  }
+}
+
+// функция отображения курсов валют
+function printCurrencies(currencies) {
+  itemsTable.innerHTML = ''; 
+  for (const key in currencies) {
+    if (currencies.hasOwnProperty(key)) {
+      const currency = currencies[key];
+
+      const divItem = document.createElement('div');
+      divItem.className = 'item';
+
+      divItem.innerHTML = `
+        <div class="item__code">${currency.CharCode}</div>
+        <div class="item__value">${currency.Value.toFixed(2)}</div>
+        <div class="item__currency">руб.</div>
+      `;
+      itemsTable.appendChild(divItem);
+    }
   }
 }
 
